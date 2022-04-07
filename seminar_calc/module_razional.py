f516278d263b0ea2dcf2d7ae58fde50f1481e758
DmitriyyLobanov <dmitriyylobanov2@gmail.com>
2022-04-07 22:16:54 +0400

    module_raz

---
+++ seminar_calc/module_razional.py
@@ -0,0 +1,27 @@
+# Модуль математических операций с рациональными числами.
+
+x = 0
+y = 0
+
+def init(a, b):
+    global x
+    global y
+    x = a
+    y = b
+
+
+
+def razi_sum():
+    return x + y
+
+def razi_mult():
+    return x * y
+
+def razi_sub():
+    return x - y
+
+def razi_dif():
+    return x / y
+
+
+

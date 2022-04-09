79ee1c0d8b81611e02b3bad0f23eb633df1510d2
Elvira09 <zag0909@gmail.com>
2022-04-09 20:42:35 +0300

    expression

--- seminar_calc/controller.py
+++ seminar_calc/controller.py
@@ -1,12 +1,9 @@
-import module_razional as model
+import module
 import view
 import logger 
 
 def button_click():
-    value_a = view.get_value()
-    math_char = view.get_math_char()
-    value_b = view.get_value()
-    model.init(value_a, value_b)
-    result = model.do_it(math_char)
+    result = module.do_it()
     view.view_data(result)
-    logger.html_logger(value_a, math_char, value_b, result)
+    logger.html_logger(result)
+
